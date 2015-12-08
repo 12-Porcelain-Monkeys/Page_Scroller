b360db6e3e9be48ec043a182b4be5d1b0ce6e4c6
12-Porcelain-Monkeys <thalaxin@aim.com>
2015-12-08 15:05:36 -0500

    change1

---
+++ Page_Scroller.user.js
@@ -0,0 +1,7 @@
+// ==UserScript==
+// @name        Page Scroller
+// @namespace   Amazon Page Scrolling
+// @include     https://gist.github.com/597d7db09fe16c832241.git
+// @version     1
+// @grant       none
+// ==/UserScript==
